23ddd6b42e6d411f262c88d5bdad35c7ce50b0d4
mohdnasir111 <122935164+mohdnasir111@users.noreply.github.com>
2024-10-12 22:47:05 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,62 @@
+let key = "d4cc825615d24733ad8abcc136e4f5cb";
+let cardData = document.querySelector(".cardData");
+let SearchBtn = document.getElementById("searchBtn");
+let inputData = document.getElementById("inputData");
+let searchType = document.getElementById("type");
+
+
+const getData = async(input) => {
+  let res = await fetch(`https://newsapi.org/v2/everything?q=${input}&apiKey=${key}`
+  );
+  let jsonData = await res.json();
+  console.log(jsonData.articles);
+  cardData.innerHTML="";
+  jsonData.articles.forEach(function(article){
+    console.log(article);
+    let divs = document.createElement("div");
+  divs.classList.add("card");
+  cardData.appendChild(divs);
+  searchType.innerHTML="Search:"+input;
+ divs.innerHTML=`
+ <img src="${article.urlToImage}" alt="">
+        <h3>${article.title}</h3>
+        <p>${article.description}</p>
+        `
+    divs.addEventListener("click",function(){
+      window.open(article.url);
+    })
+  })
+  /*let divs = document.createElement("div");
+  divs.classList.add("card");
+  cardData.appendChild(divs);
+ divs.innerHTML=`
+ <img src="${jsonData.articles[0].urlToImage}" alt="">
+        <h3>${jsonData.articles[0].title}</h3>
+        <p>${jsonData.articles[0].description}</p>
+        `*/
+};
+window.addEventListener("load", function(){
+  getData("Cricket");
+})
+SearchBtn.addEventListener("click", function(){
+let inputText = inputData.value;
+getData(inputText);
+})
+function navClick(navName){
+  if(navName =="politics"){
+    document.getElementById("politics").style.color="rgb(0,140,255)"
+    document.getElementById("sports").style.color="white"
+    document.getElementById("technology").style.color="white"
+  }
+  if(navName =="sports"){
+    document.getElementById("politics").style.color="white"
+    document.getElementById("sports").style.color="rgb(0,140,255)"
+    document.getElementById("technology").style.color="white"
+  }
+  if(navName =="technology"){
+    document.getElementById("politics").style.color="white"
+    document.getElementById("sports").style.color="white"
+    document.getElementById("technology").style.color="rgb(0,140,255)"
+  }
+  getData(navName);
+}
