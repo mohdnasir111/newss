bf0d95eb7c14e14486d3e0fb45959db3a6820006
mohdnasir111 <122935164+mohdnasir111@users.noreply.github.com>
2024-10-13 09:32:36 +0530

    Update style.js

--- style.js
+++ style.js
@@ -65,7 +65,7 @@ function navClick(navName) {
   }
   if (navName == "technology") {
     document.getElementById("politics").style.color = "white"
-     document.getElementById("sports").style.color ="white"
+     document.getElementById("sports").style.color = "white"
     document.getElementById("technology").style.color =  "rgb(0,140,255)"
   }
 getData(navName);
